0fff53ebf18dbd628ce8d70365514cfc68790a1e
Andy LeClair <andyleclair@gmail.com>
2022-10-07 13:47:50 -0400

    bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule StripJs.Mixfile do
   def project do
     [
       app: :strip_js,
-      version: "1.1.2",
+      version: "1.1.3",
       description: "Strip JavaScript from HTML and CSS",
       package: package(),
       elixir: "~> 1.10",
